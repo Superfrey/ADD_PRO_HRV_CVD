677412755bf6b68604d7995e8841693fd4f71a8b
Jonas Schaarup <60611191+Superfrey@users.noreply.github.com>
2023-08-23 15:21:40 +0200

    Update 1_generate_IBI_data.R
    Cross check with vdi

--- R/hrv_functions/1_generate_IBI_data.R
+++ R/hrv_functions/1_generate_IBI_data.R
@@ -10,7 +10,8 @@ create_time_day_variables <- function(data) {
       day = lubridate::day(datetime),
       week_day = lubridate::wday(datetime),
       hour = as.numeric(lubridate::hour(datetime)),
-      day_number = as.integer(as.Date(datetime) - min(as.Date(datetime))),
+      year_day = yday(datetime),
+      day_number = year_day-min(year_day),
       circadian_time_points = cut(hour, breaks = cut_points, labels = hour_labels, right = FALSE)
     )
 
@@ -19,39 +20,75 @@ create_time_day_variables <- function(data) {
 
 ###########
 calculate_ibi <- function(data) {
+  set.seed(1234)
 
-    set.seed(1234)
+  data <- data %>%
+    dplyr::filter(
+      timepoint >= 120,
+      dplyr::between(Mean_HR, 25, 250),
+      dplyr::between(Upper_HR, 25, 250),
+      dplyr::between(Lower_HR, 25, 250)
+    ) %>%
+    dplyr::mutate(
+      mean_ibi = 60000 / Mean_HR,
+      upper_ibi = 60000 / Lower_HR,
+      lower_ibi = 60000 / Upper_HR
+    )
 
-    data <- data %>%
-        dplyr::filter(
-            timepoint >= 0,
-            dplyr::between(Mean_HR, 25, 250),
-            dplyr::between(Upper_HR, 25, 250),
-            dplyr::between(Lower_HR, 25, 250)
-        ) %>%
-        dplyr::mutate(
-            mean_ibi = 60000 / Mean_HR,
-            upper_ibi = 60000 / Lower_HR,
-            lower_ibi = 60000 / Upper_HR
-        )
+  ibi <- data %>%
+    dplyr::group_split(timepoint) %>%
+    purrr::map_dfr(~ {
+      mean <- .x$mean_ibi
+      n <- round(30000 / mean)
+      sd <- (.x$upper_ibi - .x$lower_ibi) / (2 * 1.96)
 
-    ibi <- data %>%
-        dplyr::group_split(timepoint) %>%
-        purrr::map_dfr(~ {
-            mean <- .x$mean_ibi
-            n <- round(30000 / mean)
-            sd <- (.x$upper_ibi - .x$lower_ibi) / (2 * 1.96)
+      tibble::tibble(
+        timepoint = .x$timepoint,
+        ibi = stats::rnorm(n, mean = mean, sd = sd)
+      )
+    })
 
-            tibble::tibble(
-                timepoint = .x$timepoint,
-                ibi = stats::rnorm(n, mean = mean, sd = sd)
-            )
-        })
+  ibi_data <- data %>%
+    dplyr::full_join(ibi, by = join_by(timepoint))
 
-    ibi_data <-   data %>%
-        dplyr::full_join(ibi, by = join_by(timepoint))
-
-    return(ibi_data)
+  return(ibi_data)
 }
 
-##############
+
+############## Physical activity data
+
+numextract <- function(string){
+    str_extract(string, "\\-*\\d+\\.*\\d*")}
+
+pa_data_preperation <- function(pa_data){
+
+ pa_data <- pa_data %>%
+     dplyr::group_by(id) %>%
+     dplyr::mutate(pro_no = numextract(id)) %>%
+     dplyr::mutate(pro_no = as.numeric(pro_no))
+
+ pa_data <- pa_data %>%
+     dplyr::mutate(
+         datetime = as.POSIXct(TIMESTAMP, format = "%d-%m-%Y %H:%M:%S"),
+         day = lubridate::day(datetime),
+         week_day = lubridate::wday(datetime),
+         hour = as.numeric(lubridate::hour(datetime)),
+         year_day = yday(datetime))
+
+ pa_data <- pa_data %>%
+     group_by(pro_no) %>%
+     mutate(day_number = year_day-min(year_day),
+            day_num = dayofyear - min(dayofyear))
+
+
+  pa_data <- pa_data %>%
+     select(pro_no,datetime, hour, day_number,day_num,year_day,
+            hourofday, dayofweek, dayofyear,
+            INT_stdMET_highIC_BRANCH_cat6,INT_2MET_Branch7, SLEEP, sr_sleep) %>%
+      rename(time_frach_MET_under_25 = INT_stdMET_highIC_BRANCH_cat6,
+             time_spend_MET_above_3 = INT_2MET_Branch7)
+
+    return(pa_data)
+}
+
+#pa_data <- pa_data_preperation(hourly_means_df)
